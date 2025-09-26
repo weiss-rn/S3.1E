f61b6c80d54c8f4621d4549864dc62376bd5bd8a
Weiss_rN <darma20060311@gmail.com>
2025-09-26 11:38:43 +0700

    Create Operator-aritmatika.js

---
+++ Front-End/P2/External JS/Operator-aritmatika.js
@@ -0,0 +1,45 @@
+// Variable
+var a = 20;
+var b = 5;
+var c = 0;
+
+// Operator aritmatika
+// Penambahan
+c = a + b;
+console.log('${a} + ${b} = ${c}');
+document.write('${a} + ${b} = ${c}<br/>');
+
+// Pengurangan
+c = a - b;
+console.log('${a} - ${b} = ${c}');
+document.write('${a} - ${b} = ${c}<br/>');
+// Perkalian
+c = a * b;
+console.log('${a} * ${b} = ${c}');
+document.write('${a} * ${b} = ${c}<br/>');
+
+// Pembagian
+c = a / b;
+console.log('${a} / ${b} = ${c}');
+document.write('${a} / ${b} = ${c}<br/>');
+
+// Modulus
+c = a % b
+console.log('${a} % ${b} = ${c}');
+document.write('${a} % ${b} = ${c}<br/>');
+
+// Pemangkatan
+c = a ** b;
+console.log('${a} ** ${b} = ${c}');
+document.write('${a} ** ${b} = ${c}<br/>');
+
+// Increment
+c = a++;
+console.log('${a}++ = ${c}');
+document.write('${a}++ = ${c}<br/>');
+
+// Decrement
+c = a--;
+console.log('${a}-- = ${c}');
+document.write('${a}-- = ${c}<br/>');
+
