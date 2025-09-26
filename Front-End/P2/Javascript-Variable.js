0d0fb4c971a28caf137a7943de5b11fd6d25ead1
Weiss_rN <darma20060311@gmail.com>
2025-09-26 11:01:26 +0700

    Create Javascript-Variable.js

---
+++ Front-End/P2/Javascript-Variable.js
@@ -0,0 +1,11 @@
+var name = "Javascript";
+var visitCount = "200000";
+var isActive = true;
+var url = "https://example.com";
+
+alert("Welcome to " + name + "!");
+
+document.write("Welcome to " + name + "!" + "<br>");
+document.write("This page total visitor is " + visitCount + "<br>");
+document.write("and this page is " + (isActive ? "active" : "inactive" ) + "<br>");
+document.write("and this page url is " + url + "<br>");
