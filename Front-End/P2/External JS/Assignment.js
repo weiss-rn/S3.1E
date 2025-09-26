46a87c6cf68418a5e4ba96a56029bf1049fd17ed
Weiss_rN <darma20060311@gmail.com>
2025-09-26 18:41:51 +0700

    Create Assignment.js

---
+++ Front-End/P2/External JS/Assignment.js
@@ -0,0 +1,29 @@
+document.write("Mula‑mula milai score ...<br>");
+
+// initial value
+var score = 100;
+document.write("score = " + score + "<br>");
+
+// addition assignment (+= 5)
+score += 5;
+document.write("score after += 5: " + score + "<br>");
+
+// subtraction assignment (-= 2)
+score -= 2;
+document.write("score after -= 2: " + score + "<br>");
+
+// multiplication assignment (*= 2)
+score *= 2;
+document.write("score after *= 2: " + score + "<br>");
+
+// division assignment (/= 4)
+score /= 4;
+document.write("score after /= 4: " + score + "<br>");
+
+// exponentiation assignment (**= 2)
+score **= 2;
+document.write("score after **= 2: " + score + "<br>");
+
+// modulo assignment (%= 3)
+score %= 3;
+document.write("score after %= 3: " + score + "<br>");
