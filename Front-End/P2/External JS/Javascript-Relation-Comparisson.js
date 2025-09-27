fb575cc6f98701d33f07a66de480abbdd3d5ddea
Weiss_rN <darma20060311@gmail.com>
2025-09-27 21:24:07 +0700

    Create Javascript-Relation-Comparisson.js

---
+++ Front-End/P2/External JS/Javascript-Relation-Comparisson.js
@@ -0,0 +1,26 @@
+var me = 20;
+var you = 30;
+// EQUAL
+var result = me == you;
+console.log(me == you);
+document.write('${me} == ${you} = ${result}<br/>');
+
+// NOT EQUAL
+console.log(me != you);
+document.write('${me} != ${you} = ${result}<br/>');
+
+// GREATER THAN
+console.log(me > you);
+document.write('${me} > ${you} = ${result}<br/>');
+
+// LESS THAN
+console.log(me < you);
+document.write('${me} < ${you} = ${result}<br/>');
+
+// GREATER THAN OR EQUAL TO
+console.log(me >= you);
+document.write('${me} >= ${you} = ${result}<br/>');
+
+// LESS THAN OR EQUAL TO
+console.log(me <= you);
+document.write('${me} <= ${you} = ${result}<br/>');
