bf2274b362480987db6b61f0db4cd30fe3f435a7
Weiss_rN <darma20060311@gmail.com>
2025-11-07 15:26:06 +0900

    Update seed.sql

--- Back-End/P7/seed.sql
+++ Back-End/P7/seed.sql
@@ -1,15 +1,3 @@
--- InnoDB being an ass
--- CREATE TABLE IF NOT EXISTS stok (
---     id INT AUTO_INCREMENT PRIMARY KEY,
---     kode_sparepart VARCHAR(50) NOT NULL,
---     nama_sparepart VARCHAR(100) NOT NULL,
---     harga DECIMAL(10, 2) NOT NULL,
---     stok INT NOT NULL,
---     tipe_motor VARCHAR(100),
---     image VARCHAR(255)
--- );
-
-
 CREATE TABLE IF NOT EXISTS stok (
     id INT AUTO_INCREMENT PRIMARY KEY,
     kode_sparepart VARCHAR(50) NOT NULL,
@@ -36,4 +24,5 @@ INSERT INTO stok (kode_sparepart, nama_sparepart, harga, stok, tipe_motor, image
 ('SP012', 'Knalpot Racing', 350000.00, 6, 'Universal', NULL),
 ('SP013', 'Shockbreaker Depan', 250000.00, 9, 'Universal', NULL),
 ('SP014', 'Shockbreaker Belakang', 275000.00, 8, 'Universal', NULL),
+
 ('SP015', 'Lampu Sein LED', 25000.00, 35, 'Universal', NULL);
